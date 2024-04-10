28cc9a08e46251ce74234b1c3a6a851dda6e579e
ZJCFCB <15674683467@163.com>
2024-04-10 11:07:14 +0800

    leedcode 203 移除链表元素

--- list/203.go
+++ list/203.go
@@ -7,13 +7,31 @@ import (
 type Test203 struct{}
 
 func removeElements(head *ListNode, val int) *ListNode {
-	return head
+
+	if head == nil {
+		return nil
+	}
+
+	var forhead, behind, newHead *ListNode
+	newHead = &ListNode{0, head}
+	forhead = newHead.Next
+	behind = newHead
+	for forhead != nil {
+		if forhead.Val == val {
+			behind.Next = forhead.Next
+			forhead = behind.Next
+		} else {
+			forhead = forhead.Next
+			behind = behind.Next
+		}
+	}
+	return newHead.Next
 }
 
 func (test Test203) Run() {
 	array := []int{1, 2, 6, 3, 4, 5, 6}
 	head := NewListByArray(array)
-	result := removeElements(head, 6)
+	result := removeElements(head, 7)
 	ans := TurnListToArray(result)
 	fmt.Println(ans)
 
