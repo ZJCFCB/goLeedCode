9c374b785416713a911e2f538136beaccd912a89
ZJCFCB <15674683467@163.com>
2024-05-01 14:30:59 +0800

    leedcode 669 修剪二叉搜索树,（注意审题，解答妙哉）

--- binaryTree/669.go
+++ binaryTree/669.go
@@ -18,6 +18,31 @@ func trimBST(root *TreeNode, low int, high int) *TreeNode {
 	return root
 }
 
+/*
+好好看看，，，，什么是优雅，，不复杂。注意审题。。
+不要把问题想复杂了，，，根本就没必要挨个删掉阿。。
+因为题目没有要求，，，修剪后的树仍然是一颗AVL树。。。
+
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val < low {
+		return trimBST(root.Right, low, high)
+	}
+	if root.Val > high {
+		return trimBST(root.Left, low, high)
+	}
+	root.Left = trimBST(root.Left, low, high)
+	root.Right = trimBST(root.Right, low, high)
+	return root
+}
+
+
+
+
+*/
+
 func (T Test669) Run() {
 	nums1 := []int{3, 0, 4, -1, 2, -1, -1, -1, -1, 1}
 	t1 := NewTree(nums1)
