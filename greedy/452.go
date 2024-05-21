68bbc4e9e16d69627b68fa5b3c5c17531f81d2c0
ZJCFCB <15674683467@163.com>
2024-05-21 14:53:15 +0800

    leedcode 452  用最少数量的箭引爆气球

--- greedy/452.go
+++ greedy/452.go
@@ -37,6 +37,28 @@ func findMinArrowShots(points [][]int) int {
 	return ans
 }
 
+/*
+
+别人的代码优雅一点
+
+func findMinArrowShots(points [][]int) int {
+    if len(points) == 0 {
+        return 0
+    }
+    sort.Slice(points, func(i, j int) bool { return points[i][1] < points[j][1] })
+    maxRight := points[0][1]
+    ans := 1
+    for _, p := range points {
+        if p[0] > maxRight {
+            maxRight = p[1]
+            ans++
+        }
+    }
+    return ans
+}
+
+*/
+
 func (T Test452) Run() {
 	var people [][]int = [][]int{
 		{3, 9}, {7, 12}, {3, 8}, {6, 8}, {9, 10}, {2, 9}, {0, 9}, {3, 9}, {0, 6}, {2, 8},
