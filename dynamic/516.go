9ac1dc669680dad81fe1769ab126c0f9cc32f768
ZJCFCB <15674683467@163.com>
2024-07-26 14:38:19 +0800

    leedcode 516. 最长回文子序列

---
+++ dynamic/516.go
@@ -0,0 +1,34 @@
+package dynamic
+
+import "fmt"
+
+type Test516 struct{}
+
+func longestPalindromeSubseq(s string) int {
+	size := len(s)
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	dp := make([][]int, size)
+	for i := 0; i < size; i++ {
+		dp[i] = make([]int, size)
+		dp[i][i] = 1
+	}
+	for i := size - 1; i >= 0; i-- {
+		for j := i + 1; j < size; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i][j-1], dp[i+1][j])
+			}
+		}
+	}
+	return dp[0][size-1]
+}
+
+func (T Test516) Run() {
+	fmt.Println(longestPalindromeSubseq("abcdffcba"))
+}
--- main.go
+++ main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 
-	util.ListTest(dynamic.Test471{})
+	util.ListTest(dynamic.Test516{})
 
 }
